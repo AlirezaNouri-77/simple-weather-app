f65db6efaa812a5f7bf7395805625d160aa0fcb8
Shermanrex2 <71293106+Shermanrex2@users.noreply.github.com>
2021-03-30 20:42:13 +0430

    Initial commit

---
+++ app/src/main/java/com/example/weathertest/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.weathertest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
