707662dbf6be6fd74a15aaa4bcbf2ce3e6f339be
Shermanrex2 <71293106+Shermanrex2@users.noreply.github.com>
2021-09-27 20:29:58 +0330

    change api

--- app/src/main/java/com/example/weathertest/MainActivity.java
+++ app/src/main/java/com/example/weathertest/MainActivity.java
@@ -1,28 +1,20 @@
 package com.example.weathertest;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.ActionBar;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Adapter;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.weathertest.model.recyclerview_item;
 import com.example.weathertest.recyckerview.RecyclerviewAdapter;
@@ -32,7 +24,6 @@ import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -45,9 +36,6 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity implements RecyclerviewAdapter.Onitemclick {
-
-//    String url = "https://api.openweathermap.org/data/2.5/weather?q=tehran&units=metric&appid=e5085a29070720b8ee6224e489e9a63a";
-//    String url2 = "https://api.openweathermap.org/data/2.5/forecast?q=tehran&units=metric&cnt=10&appid=e5085a29070720b8ee6224e489e9a63a";
 
 
     List<recyclerview_item> list;
@@ -65,16 +53,16 @@ public class MainActivity extends AppCompatActivity implements RecyclerviewAdapt
 
         list = new ArrayList<>();
 
-        forcast();
-        currentweather();
+        curent_weather();
+        forcast_weather();
 
     }
 
     @Override
     protected void onStart() {
         list.clear();
-        forcast();
-        currentweather();
+        curent_weather();
+        forcast_weather();
         super.onStart();
 
 
@@ -82,9 +70,9 @@ public class MainActivity extends AppCompatActivity implements RecyclerviewAdapt
 
     @Override
     protected void onResume() {
-         list.clear();
-        forcast();
-        currentweather();
+        list.clear();
+        curent_weather();
+        forcast_weather();
         super.onResume();
 
     }
@@ -129,8 +117,6 @@ public class MainActivity extends AppCompatActivity implements RecyclerviewAdapt
         intent.putExtra("time", list.get(p).time);
         intent.putExtra("min", list.get(p).min);
         intent.putExtra("max", list.get(p).max);
-        intent.putExtra("pressure", list.get(p).pressure);
-        intent.putExtra("humidity", list.get(p).humidity);
         intent.putExtra("iconurl", list.get(p).icon);
 
         startActivity(intent);
@@ -138,16 +124,15 @@ public class MainActivity extends AppCompatActivity implements RecyclerviewAdapt
     }
 
     public String urlmaker() {
-        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + getlocation() + "&units=metric&appid=e5085a29070720b8ee6224e489e9a63a";
-        return url;
+        return "https://api.weatherbit.io/v2.0/current?city=" + getlocation() + "&key=8a8ec0d5af5f4806ada672017c6d44b5";
     }
 
     public String urlmaker2() {
-        return "https://api.openweathermap.org/data/2.5/forecast?q=" + getlocation() + "&units=metric&cnt=10&appid=e5085a29070720b8ee6224e489e9a63a";
+        return "https://api.weatherbit.io/v2.0/forecast/daily?city=" + getlocation() + "&key=8a8ec0d5af5f4806ada672017c6d44b5";
     }
 
 
-    public void forcast() {
+    public void curent_weather() {
 
         cityname = findViewById(R.id.cityname);
         currenttemp = findViewById(R.id.currenttemp);
@@ -179,22 +164,16 @@ public class MainActivity extends AppCompatActivity implements RecyclerviewAdapt
 
                             try {
                                 JSONObject jsonObject = new JSONObject(response.body().string());
-                                JSONArray jsonArray = jsonObject.getJSONArray("weather");
+                                JSONArray jsonArray = jsonObject.getJSONArray("data");
                                 for (int j = 0; j < jsonArray.length(); j++) {
                                     JSONObject s = jsonArray.getJSONObject(j);
-                                    iconcode = s.getString("icon");
-                                    condition.setText(s.get("main") + "");
-
+                                    iconcode = s.getJSONObject("weather").getString("icon");
+                                    condition.setText(String.valueOf(s.getJSONObject("weather").getString("description")));
+                                    cityname.setText(s.getString("city_name").toUpperCase());
+                                    currenttemp.setText(Math.round(Float.parseFloat(s.getString("temp"))) + " \u2103");
+                                    Picasso.get().load("https://www.weatherbit.io/static/img/icons/" + s.getJSONObject("weather").getString("icon") + ".png").into(imageView);
                                     break;
                                 }
-
-                                Picasso.get().load("https://openweathermap.org/img/wn/" + iconcode + "@2x.png").into(imageView);
-
-                                currenttemp.setText(Math.round(Float.parseFloat(jsonObject.getJSONObject("main").getString("temp"))) + " \u2103");
-                                mintemp.setText("Min : " + Math.round(Double.parseDouble(jsonObject.getJSONObject("main").getString("temp_min"))) + " \u2103");
-                                higttemp.setText("Max : " + Math.round(Double.parseDouble(jsonObject.getJSONObject("main").getString("temp_max"))) + " \u2103");
-
-                                cityname.setText(jsonObject.getString("name").toUpperCase());
 
                             } catch (JSONException | IOException e) {
                                 e.printStackTrace();
@@ -209,12 +188,14 @@ public class MainActivity extends AppCompatActivity implements RecyclerviewAdapt
         });
     }
 
-    public void currentweather() {
-
+    public void forcast_weather() {
 
         cardView = findViewById(R.id.cardView2);
         recyclerView = findViewById(R.id.RV);
-
+        fail = findViewById(R.id.fail);
+        fail.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.VISIBLE);
+        cardView.setVisibility(View.VISIBLE);
 
         OkHttpClient okHttpClient1 = new OkHttpClient();
         Request request1 = new Request.Builder()
@@ -234,51 +215,18 @@ public class MainActivity extends AppCompatActivity implements RecyclerviewAdapt
 
                 if (response.isSuccessful()) {
 
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-
-                            fail = findViewById(R.id.fail);
-                            fail.setVisibility(View.GONE);
-                            recyclerView.setVisibility(View.VISIBLE);
-                            cardView.setVisibility(View.VISIBLE);
-
-                        }
-                    });
-
-
-
                     try {
 
                         jsonObject = new JSONObject(response.body().string());
-
-
-                        JSONArray jsonArray = jsonObject.getJSONArray("list");
-
-
+                        JSONArray jsonArray = jsonObject.getJSONArray("data");
                         for (int i = 0; i < jsonArray.length(); i++) {
-
-                            String code = "";
-
                             JSONObject j = jsonArray.getJSONObject(i);
-                            JSONArray array = j.getJSONArray("weather");
-                            for (int s = 0 ; s < array.length() ; s++){
-                                JSONObject as = array.getJSONObject(s);
-                             code = as.getString("icon");
-                            }
-
-
-                            String url = "https://openweathermap.org/img/wn/"+code+"@2x.png";
-                            list.add(new recyclerview_item(j.getJSONObject("main").getString("temp"),
-                                            j.getString("dt_txt"),
-                                            j.getJSONObject("main").getString("temp_min"),
-                                            j.getJSONObject("main").getString("temp_max"),
-                                            j.getJSONObject("main").getString("pressure"),
-                                            j.getJSONObject("main").getString("humidity"),
-                                            url
-
-                                    )
-
+                            String url = "https://www.weatherbit.io/static/img/icons/"+ j.getJSONObject("weather").getString("icon") + ".png";
+                            list.add(new recyclerview_item(j.getString("temp"),
+                                    j.getString("valid_date"),
+                                    j.getString("max_temp"),
+                                    j.getString("low_temp"),
+                                    url)
                             );
                         }
 
--- app/src/main/java/com/example/weathertest/model/recyclerview_item.java
+++ app/src/main/java/com/example/weathertest/model/recyclerview_item.java
@@ -5,18 +5,14 @@ public class recyclerview_item {
     public String temp;
     public String icon;
     public String min;
-    public String pressure;
-    public String humidity;
     public String max;
     public String time;
 
-    public recyclerview_item(String temp, String time, String max, String min , String pressure , String humidity ,String icon) {
+    public recyclerview_item(String temp, String time, String max, String min ,String icon) {
         this.temp = temp;
         this.time = time;
         this.max = max;
         this.icon=icon;
-        this.pressure=pressure;
-        this.humidity=humidity;
         this.min = min;
     }
 
